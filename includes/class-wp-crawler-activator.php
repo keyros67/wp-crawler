9a86b1c2118d6486b5e95acf67da7e2909e8aeec
Hervé Villemin <herve.villemin@keyros.com>
2021-03-19 14:38:07 -0500

    chore: add wordpress plugin template

---
+++ includes/class-wp-crawler-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/villemin/
+ * @since      1.0.0
+ *
+ * @package    Wp_Crawler
+ * @subpackage Wp_Crawler/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Crawler
+ * @subpackage Wp_Crawler/includes
+ * @author     Hervé Villemin <herve@villemin.co>
+ */
+class Wp_Crawler_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-wp-crawler-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.linkedin.com/in/villemin/
+ * @since      1.0.0
+ *
+ * @package    Wp_Crawler
+ * @subpackage Wp_Crawler/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Crawler
+ * @subpackage Wp_Crawler/includes
+ * @author     Hervé Villemin <herve@villemin.co>
+ */
+class Wp_Crawler_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
